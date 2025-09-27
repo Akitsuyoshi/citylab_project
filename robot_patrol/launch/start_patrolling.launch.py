4b83fd1abdd9cd018dcd911b43cc671e1163593d
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-09-27 01:01:57 +0000

    Add rviz2 in launch and its configuration

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,10 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='robot_patrol',
             executable='robot_patrol_node',
-            output='screen')
+            output='screen'),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(get_package_share_directory('robot_patrol'), 'config', 'config.rviz')]
+            ),
     ])
