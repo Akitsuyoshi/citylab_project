1cddd7455c0abeeb27534138226a8239a4826f9d
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-10-02 08:34:57 +0000

    Add a custom interface pkg for direction service

--- robot_patrol/src/direction_service.cpp
+++ robot_patrol/src/direction_service.cpp
@@ -1,24 +1,26 @@
+#include "custom_interfaces/srv/get_direction.hpp"
+#include "rclcpp/logging.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include <std_srvs/srv/trigger.hpp>
 
 class DirectionService : public rclcpp::Node {
+  using GetDirection = custom_interfaces::srv::GetDirection;
+
 public:
   DirectionService() : Node("direction_service_server") {
     std::string service_n = "/direction_service";
-    service_ = create_service<std_srvs::srv::Trigger>(
-        service_n,
-        [this](const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
-               std::shared_ptr<std_srvs::srv::Trigger::Response> response) {
+    service_ = create_service<GetDirection>(
+        service_n, [this](const std::shared_ptr<GetDirection::Request> request,
+                          std::shared_ptr<GetDirection::Response> response) {
           return service_callback(request, response);
         });
+    RCLCPP_INFO(get_logger(), "Service Server Ready");
   }
 
 private:
-  void service_callback(
-      const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
-      std::shared_ptr<std_srvs::srv::Trigger::Response> response) {}
+  void service_callback(const std::shared_ptr<GetDirection::Request> request,
+                        std::shared_ptr<GetDirection::Response> response) {}
 
-  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr service_;
+  rclcpp::Service<GetDirection>::SharedPtr service_;
 };
 
 int main(int argc, char **argv) {
